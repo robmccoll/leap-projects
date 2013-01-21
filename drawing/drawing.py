c17328956c336704f8b1148063cfa14c7c80492b
Rob McColl <rob@robmccoll.com>
2013-01-21 13:03:47 -0600

    Adding continuous drawing
    Adds continuous drawing and fixes async previous bug

--- drawing/drawing.py
+++ drawing/drawing.py
@@ -26,6 +26,11 @@ class ScaledVector(Leap.Vector):
         self.y = self.interpolate(val.y, self.scale.y, self.in_max.y, self.in_min.y, self.in_range.y)
         self.z = self.interpolate(val.z, self.scale.z, self.in_max.z, self.in_min.z, self.in_range.z)
 
+    def copy_to_vec(self, vec):
+        vec.x = self.x
+        vec.y = self.y
+        vec.z = self.z
+
 class OneFinger(Leap.Listener):
     def __init__(self, location):
         Leap.Listener.__init__(self)
@@ -35,19 +40,19 @@ class OneFinger(Leap.Listener):
         frame = controller.frame()
         if not frame.hands.empty and not frame.hands[0].fingers.empty:
             fingers = frame.hands[0].fingers
-            if len(fingers) == 1:
-                self.location.set_location(fingers[0].tip_position)
+            self.location.set_location(fingers[0].tip_position, len(fingers) == 1)
 
 class LocationPainter:
     def __init__(self, panel, scaledvec):
         self.loc = scaledvec
+        self.prev = Leap.Vector(scaledvec)
         self.on = self.loc.scale.z * 0.5
         self.bmp = wx.EmptyBitmap(self.loc.scale.x, self.loc.scale.y)
         self.dc = wx.MemoryDC()
         self.dc.SelectObject(self.bmp)
         self.panel = panel
 
-    def set_location(self, new_loc):
+    def set_location(self, new_loc, erase):
         self.loc.set(new_loc)
         self.panel.Refresh()
 
@@ -59,13 +64,14 @@ class LocationPainter:
             dc.DrawLine(self.loc.x,self.loc.y, self.loc.x, self.loc.y)
         else:
             self.dc.SetPen(wx.Pen("BLUE", 10))
-            self.dc.DrawLine(self.loc.x,self.loc.y, self.loc.x, self.loc.y)
+            self.dc.DrawLine(self.prev.x,self.prev.y, self.loc.x, self.loc.y)
+        self.loc.copy_to_vec(self.prev)
 
 def main():
-    init_pos = Leap.Vector(500, 400, 0)
+    init_pos = Leap.Vector(500, 400, 100)
     img_size = Leap.Vector(1000, 800, 100)
-    phys_min = Leap.Vector(-90, 360, -180)
-    phys_max = Leap.Vector(130, 130, 60)
+    phys_min = Leap.Vector(-70, 360, -180)
+    phys_max = Leap.Vector(100, 130, 60)
     scaled_vec = ScaledVector(init_pos, img_size, phys_min, phys_max)
     
     # Create the wxApp and frame
